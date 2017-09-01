53177715e79f725356ab350bdfa599bb20afdbb7
Tyler Mann <tjmann95@gmail.com>
2017-09-01 09:51:46 -0400

    Switch between creating new windows and not

--- Simple PDF Editor/bingsearch.py
+++ Simple PDF Editor/bingsearch.py
@@ -6,8 +6,7 @@ import time
 browser = webdriver.Chrome(executable_path="C:\\Users\\1117254\\Documents\\chromedriver.exe")
 browser.set_window_size(1920, 1080)
 
-
-def login(username, password):
+def login(username, password, browser):
     browser.get("http://www.bing.com")
     time.sleep(2)
     sign_in = browser.find_element_by_id("id_a")
@@ -29,7 +28,7 @@ def login(username, password):
     enter.click()
 
 
-def do_search(word):
+def do_search(word, browser):
     search_box = browser.find_element_by_id("sb_form_q")
     search_box.send_keys(word)
     search_box.submit()
@@ -37,7 +36,7 @@ def do_search(word):
     search_box.clear()
 
 
-def logout():
+def logout(browser):
     logout_button = browser.find_element_by_id("id_l")
     logout_button.click()
     time.sleep(2)
@@ -45,7 +44,7 @@ def logout():
     logout.click()
 
 
-def get_points():
+def get_points(browser):
     points_elem = browser.find_element_by_id("id_rc")
     return points_elem.text
 
@@ -60,19 +59,23 @@ def main():
     start_time = time.time()
     for each_login in range(len(usernames)):
         if usernames[each_login]:
+            # browser = webdriver.Chrome(executable_path="C:\\Users\\1117254\\Documents\\chromedriver.exe")
+            # browser.set_window_size(1920, 1080)
             print(usernames[each_login] + "...")
-            login(usernames[each_login], passwords[each_login])
-            for i in range(40):
+            login(usernames[each_login], passwords[each_login], browser)
+            for i in range(30):
                 if i == 0:
-                    print(usernames[each_login] + " has " + get_points() + " points.")
-                do_search(random.choice(words) + " " + random.choice(words))
+                    time.sleep(1)
+                    print(usernames[each_login] + " has " + get_points(browser) + " points.")
+                do_search(random.choice(words) + " " + random.choice(words), browser)
                 time.sleep(random.uniform(.5, 5.0))
-            print(usernames[each_login] + " has " + get_points() + " points.")
-            logout()
+            print(usernames[each_login] + " has " + get_points(browser) + " points.")
+            logout(browser)
             print(usernames[each_login] + " is done.\n")
     duration = time.time() - start_time
     print("Search program took " + str(int(duration / 60)) + " minutes " + str(int(duration % 60)) + " seconds to complete.")
     wordfile.close()
+    loginfile.close()
 
 if __name__ == "__main__":
     main()
